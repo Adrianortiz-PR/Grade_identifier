abec9cd82ed6bfae8ea9d72a016b4f1b1a93b05c
Adrian Ortiz <113073917+Adrianortiz-PR@users.noreply.github.com>
2022-09-14 18:29:06 -0400

    Add files via upload

---
+++ Grade identifier.py
@@ -0,0 +1,23 @@
+Grade=int(input("Your score? "))
+if Grade>= 93 and Grade<= 100: 
+  print("A+")
+elif Grade >=90 and Grade <= 92:
+  print("A")
+if Grade >= 87 and Grade <= 89:
+  print("B+")
+elif Grade >= 83 and Grade <= 86:
+  print("B")
+if Grade >= 80 and Grade <= 82:
+  print("B-")
+if Grade >= 77 and Grade <= 79:
+  print("C+")
+elif Grade >= 73 and Grade <=76:
+  print("C")
+if Grade >=67 and Grade <= 69:
+  print("D+")
+elif Grade >=63 and Grade <=66:
+  print("D")
+if Grade >=60 and Grade <= 62:
+  print("D-")
+elif Grade <= 59:
+  print("F")
